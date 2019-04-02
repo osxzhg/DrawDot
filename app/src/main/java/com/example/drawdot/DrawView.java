bb2b2a4a8a37267c69e7e1ab4024a0851b17d370
osxzhg <osxzhg@gmail.com>
2019-04-02 16:37:16 +1300

    Draw dot

--- app/src/main/java/com/example/drawdot/DrawView.java
+++ app/src/main/java/com/example/drawdot/DrawView.java
@@ -1,25 +1,48 @@
 package com.example.drawdot;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class DrawView extends View implements View.OnTouchListener {
+    ArrayList<MyPoint> points = new ArrayList<MyPoint>();
+
     public DrawView(Context context) {
         super(context);
+        setOnTouchListener(this);
     }
 
     public DrawView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setOnTouchListener(this);
     }
 
     public DrawView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        setOnTouchListener(this);
     }
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
+        points.add(new MyPoint(event.getX(),event.getY()));
+        invalidate();
         return true;
+    }
+
+    protected void onDraw(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setColor(Color.GREEN);
+        for(MyPoint pt: points){
+            canvas.drawCircle(pt.x, pt.y, 50, paint);
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/drawdot/MyPoint.java
@@ -0,0 +1,16 @@
+package com.example.drawdot;
+
+import android.graphics.PointF;
+
+public class MyPoint {
+    float x;
+    float y;
+    float size;
+    int color;
+    int StepNum;
+    int id;
+    public MyPoint(float x, float y){
+        this.x = x;
+        this.y=y;
+    }
+}
